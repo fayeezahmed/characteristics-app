4b2edde70da4497f882e96bb0c607b5936b6d23b
Fayeez Ahmed <fayeez.ahmed@MacBook-Pro-4.local>
2019-02-22 16:19:21 +0000

    Added patch functionality

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 // https://medium.com/codingthesmartway-com-blog/the-mern-stack-tutorial-building-a-react-crud-application-from-start-to-finish-part-2-637f337e5d61
 // TODO: To find out how to update individual db elements
+// START mongodb first: brew services start mongodb
+// then do node server; then do mongod --port PORT_NUM
+// Finally node server.js
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -49,7 +52,23 @@ characteristicsRoutes.route('/deleteAll').delete((req, res) => {
     characteristics.deleteMany({}, () => {
         res.status(200).json({'characteristics': 'deleted all entries!'})
     })
-})
+});
+
+characteristicsRoutes.route('/updateOne').patch((req, res) => {
+    let updateTrait = req.body;  
+    let id = req.params.id;
+    console.log(req.body)
+    characteristics.updateOne(
+            {"characteristics_type": req.body.characteristics_type},
+            {$set: updateTrait}
+        )
+        .then(characteristics => {
+            res.status(200).json(req.body)
+        })
+        .catch(err => {
+            res.status(400).send('updating one field failed')
+        })
+});
 
 app.use('/characteristics', characteristicsRoutes);
 
