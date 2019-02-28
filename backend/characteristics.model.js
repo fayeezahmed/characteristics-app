a5f47bc7f4fcb76a1d29ee5491f6604ee9ca822d
Fayeez Ahmed <fayeez.ahmed@l01007092.ptv.com>
2019-02-28 09:39:14 +0000

    WIP - need to figure out how to display the data correctly on the UI

--- backend/characteristics.model.js
+++ backend/characteristics.model.js
@@ -1,29 +1,31 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let characteristics = new Schema({
- traitName: {
+    traitName: {
      type: String
- },
- mon: {
-     type: Number
- },
- tue: {
-     type: Number
- },
- wed: {
-     type: Number
- },
- thu: {
-     type: Number
- },
- fri: {
-     type: Number
- },
- sat: {
-     type: Number
- },
- sun: {
-     type: Number
- }
+    },
+    days: {  
+        mon: {
+            type: Number
+        },
+        tue: {
+            type: Number
+        },
+        wed: {
+            type: Number
+        },
+        thu: {
+            type: Number
+        },
+        fri: {
+            type: Number
+        },
+        sat: {
+            type: Number
+        },
+        sun: {
+            type: Number
+        }
+    }
 });
 module.exports = mongoose.model('characteristics', characteristics);
--- src/Traits.js
+++ src/Traits.js
@@ -3,7 +3,17 @@ import './Traits.css';
 
 import shortid from 'shortid';
 var _ = require('lodash');
- 
+
+ function listTraitDayResults(dayResult) {
+      console.log('listTraigh <<<<<<<<<<<<<<<,')
+          return <Fragment>
+                <li key={shortid.generate()} onClick={console.log('hello')}>
+                     {dayResult}
+                </li>
+                </Fragment>
+      }
+
+
 // https://www.quackit.com/html/html_editors/scratchpad/?example=/css/flexbox/examples/flexbox_12_column_grid_2
 // https://www.quackit.com/css/flexbox/examples/
 class Traits extends Component {
@@ -12,6 +22,7 @@ class Traits extends Component {
     this.increaseCount = this.increaseCount.bind(this);
     this.createTraits = this.createTraits.bind(this);
 
+    this.listTraitName = this.listTraitName.bind(this);
     this.allTraits = this.props.allTraits;
 
     let combinedTraitsAndDays = _.zipObject(this.allTraits, _.map(this.allTraits, () => {
@@ -25,41 +36,92 @@ class Traits extends Component {
         Sun : 0}
     } ))
   
-    this.state = combinedTraitsAndDays;
+    this.state = [];
+    console.log(JSON.stringify(this.state));
   }
    increaseCount(trait, day){
-    let allTraits = this.state;
-    allTraits[trait][day] += 1
+       let allTraits = this.state;
+       let numOfTraits = Object.keys(allTraits).length
+       for (var i=0; i < numOfTraits; i++){
+           if(allTraits[i]['traitName'] == trait){
+               allTraits[i][day] += 1
+           }
+       }
 
-    this.setState(
-        allTraits[trait]
-    )
+        this.setState(
+            allTraits[trait]
+        )
   }
-    
-  createTraits(trait) {
+
+    componentDidMount() {
+        console.log('COMPONENT MOUNTING')
+        fetch('http://localhost:5000/characteristics')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log("RESULT")
+                    this.setState(result)
+                    console.log(this.state)
+                    console.log('after result')
+            })
+    }
+
+  listTraitName(traitName, days) { 
+        console.log('fhaifhlshfla')
+      return <Fragment> <li key={shortid.generate()} className="trait" data-test-id={traitName}>
+          {traitName}
+          </li>
+          <li> days</li>
+           </Fragment> }
+    createTraits() {
     // TODO: Need to save state
-    const days = ['Mon', 'Tues', 'Wed', 'Thurs',
-                  'Fri', 'Sat', 'Sun']
+    // let foundTrait = this.state.filter(traits => {
+    //    return traits.traitName == trait
+    //})
+    console.log('CREATE TRAITS')
+    let currentState = this.state
+      return <Fragment key={shortid.generate()}>
+        <ul key={shortid.generate()} className="traits">
+          { Object.keys(currentState).map(index => {
+              const traitName = currentState[index]["traitName"];
+              const days = currentState[index]["days"]
+              return this.listTraitName(traitName, days)
+              Object.keys(currentState[index]["days"]).forEach((day, value)=> {
+                  console.log('su')
+                    const dayResult = currentState[index]["days"][day]
+                  {listTraitDayResults(index, day)}
+              })
+          })
+      }      
+        </ul>
+    </Fragment>
+          ;
+          {/*
+          
     return <Fragment key={shortid.generate()}>
-      <ul key={shortid.generate()} className="traits">
-        <li key={shortid.generate()} className="trait" data-test-id={trait}>
-            {trait}
-        </li>
-      
-        {days.map((day) => 
-            <li
-                key={shortid.generate()} 
-                onClick={() => this.increaseCount(trait, day)}
-            >
+          <ul key={shortid.generate()} className="traits">
+              <li key={shortid.generate()} className="trait" data-test-id={trait}>
+              {trait}
+              </li>
+              {days.map((day) => 
+                  <li
+                  key={shortid.generate()} 
+                  onClick={() => this.increaseCount(trait, day)}
+                  >
+                  {/*
             {this.state[trait][day]}
-            </li>
-        )}
-      </ul> 
-    </Fragment>       
+            }
+                  {
+                      (this.state != [] ? this.state[0][day] : 0)
+                  }
+                  </li>
+              )}
+
+              </ul>*/ }
   }
  
   render() {
-    const listTraits = this.allTraits.map(this.createTraits);
+    const listTraits = this.createTraits(); // this.allTraits.map(this.createTraits);
     return (
       <div key={shortid.generate()}>
         {listTraits}
